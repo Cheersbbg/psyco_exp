6a091c977626ca310463e7e9818488aeca4f11c9
cheersbbg <83891351+Cheersbbg@users.noreply.github.com>
2024-01-18 17:39:03 -0800

    update and clean

--- experiments/fingers_multipleprocess.py
+++ experiments/fingers_multipleprocess.py
@@ -1,91 +1,71 @@
-import psychopy
-psychopy.useVersion('2022.1.0')
+from PIL import Image, ImageChops
+from expfunctions import *
+from natsort import natsorted
+from os.path import basename
+from pathlib import Path
 from psychopy import core, visual, event,gui, sound
 from pylsl import StreamInfo, StreamOutlet
-from PIL import Image, ImageChops
-from pathlib import Path
-import glob
-from pathlib import Path
-from natsort import natsorted
-import random
-import numpy as np
-import cv2
 from zipfile import ZipFile
-import os
-from os.path import basename
-import datetime
-import pathlib
-import pylsl
-import threading
-import queue
-import pickle
 import csv
-from expfunctions import *
+import cv2
+import datetime
+import glob
+import numpy as np
+import os
+import pathlib
+import pickle
+import psychopy
+import pylsl
+import queue
+import random
+import threading
+import time
+
+psychopy.useVersion('2022.1.0')
 
 testing_with_wifi = True
 shared_queue=queue.Queue(maxsize=1)
 now = datetime.datetime.now()
-
 exp_running=True
-
-import time
-from pylsl import StreamInfo, StreamOutlet
-
 #Crown-215
 beep1 = sound.Sound(
     value = 'A', secs = 0.2,
     volume = 0.5)
-
-
 def main():
-
     # experiment information 
     numsecs=2
-    numexp=20
+    numexp=10
     numrest=1
     exp_running = True
-
    
     if testing_with_wifi == True:
          #Set up LabStreamingLayer stream.
         print("looking for streams")
         streams_EEG= pylsl.resolve_byprop("name", "openbci_eeg",timeout=5) #Crown-215
         #streams_EEG= pylsl.resolve_byprop("name", "Crown-215",timeout=5)
-
-
         #streams_AUX=pylsl.resolve_byprop("name", "openbci_aux",timeout= 5)
-
         if len(streams_EEG) == 0:
             print("Could not find stream on the network.")
             return
             
-
         eeg_inlet = pylsl.StreamInlet(streams_EEG[0])
         #aux_inlet =  pylsl.StreamInlet(streams_AUX[0])
-
         sample, timestamp = eeg_inlet.pull_sample()
         print(sample, timestamp)
-
         #Get the sampling rate of the EEG LSL stream
         eeg_sample_rate = int(streams_EEG[0].nominal_srate())
-
         # Define the duration of the EEG and AUX data to save in seconds
         duration = numsecs+numrest
         print(eeg_sample_rate)
-
         # Define the number of samples to save
         eeg_num_samples = int(duration * eeg_sample_rate)
     #aux_num_samples = int(duration * aux_sample_rate)
-
     # Create an outlet for the second stream
     #stream_info = pylsl.StreamInfo('combined', 'EEG', 17, 0, 'float32', 'example_stream_out_001') #9 for crown
     #outlet = pylsl.StreamOutlet(stream_info)
-
     #input("Make sure it's recording then press enter...")
-
     #Instantiate the PsychoPy window and stimuli.
     win = visual.Window([1512, 982], allowGUI=None, fullscr=True, monitor='testMonitor',units='deg')
-
     #load path, load images
     thispath='/Users/qianxigong/Downloads/introspection'
     contours = np.load('handcountours.npy', allow_pickle=True)
@@ -97,17 +77,12 @@ def main():
     "lefttoright": os.getcwd()+"/exp_allfingerlefttoright[[35, 25, 11], [19, 31, 21], [15, 17, 13], [27, 29, 23], [37, 39, 33], [36, 38, 32], [26, 28, 22], [14, 16, 12], [18, 30, 20], [34, 24, 10]]",
     "righttoleft": os.getcwd()+"/exp_allfingerrighttoleft[[34, 24, 10], [18, 30, 20], [14, 16, 12], [26, 28, 22], [36, 38, 32], [37, 39, 33], [27, 29, 23], [15, 17, 13], [19, 31, 21], [35, 25, 11]]",
     "singlefinger": os.getcwd()+"/exp_eachfinger[[35, 25, 11], [19, 31, 21], [15, 17, 13], [27, 29, 23], [37, 39, 33], [36, 38, 32], [26, 28, 22], [14, 16, 12], [18, 30, 20], [34, 24, 10]]",
-    
     }
     duration_per_run = [30, 30, 30]
-
     exp_names = ["singlefinger"]
   
     new_exp_dic = {"updown": [[35,19,15,27,37,36,26,14,18,34],[25,24,31,17,29,39,38,28,16,30],[11,21,13,23,33,32,22,12,20,10],[6,7,1,0,5,4,9,8],[3,2]]}
-
-
     stim_matrix=[]
-
     for exp_name in exp_names:
         if exp_name not in exp_lib.keys():
             # create experiment directory if it doesn't exist
@@ -130,20 +105,16 @@ def main():
             pathlist = getpicpaths(exp_lib[exp_name])
             stim_list=getstims(pathlist, window=win)
             stim_matrix.append(stim_list)
-
     print("stim_matrix", len(stim_matrix[0]))
-
-
     emptystim=visual.ImageStim(win=win, image="emptyhand.png", pos = [0,0])
     emptystim2 = visual.ImageStim(win=win, image="twohands2.png", pos = [0,0])
-
     # experiment goes visual(2 second), see it then close eye, then hear beap to perform task, then second beep stop and open eyes then break for one second
-
-
-    def run_exp(exp_name,stim_list, numexp, win, timedif, shuffle=False, emptystim=None, hasbreak=False, numrest=0, break_interval=2,trial_interval=8):
+    def run_exp(exp_name,stim_list, numexp, win, timedif, shuffle=False, emptystim=None, hasbreak=False, numrest=0, break_interval=0.5,trial_interval=8):
     #keys = event.waitKeys(keyList=['space'])
     #if 'space' in keys:
         permu_list=list(range(0,len(stim_list)))
+        permu_list.append(400) #blank stimulus is 200
+        permu_list.append(400)
         core.wait(3.5)
         #timer = core.Clock()
         exp_running=True
@@ -152,57 +123,31 @@ def main():
             if shuffle == True:
                 random.shuffle(permu_list)
                 print(len(permu_list), permu_list)
-            
-            for idx in range(0, len(permu_list), 2):
-                print("idx pair:", idx, idx+1)
-            # Treat current and next stimulus as a pair
-                stim1 = stim_list[permu_list[idx]]
-                stim2 = stim_list[permu_list[idx + 1]] if idx + 1 < len(permu_list) else None
-                stim1.draw()
-
+            for idx in permu_list:
+                if idx == 400:
+                    stim = emptystim
+                else:  
+                    stim = stim_list[idx]
+                print(idx)
+                stim.draw()
                 trial_multiplier = random.randint(1, trial_interval)  # This will give you a number between 1 and 8
-                numsecs =2
-
-                win.flip()
-                core.wait(numsecs/2) #visual presentation seconds
-                stim2.draw()
-                win.flip()
-                core.wait(numsecs/2) #visual presentation seconds
-                emptystim2.draw() # background while doing it
-    
+                numsecs = trial_multiplier * 0.5 
+                # core.wait(1) #visual presentation seconds
+                # emptystim2.draw() # background while doing it
                 system_time = time.time()  # Get the current system time
                 stim_timestamp = system_time - time_diff  # Calculate the stimulus onset time using the time difference
-                shared_queue.put([permu_list[idx], stim_timestamp, trial_multiplier])
-                shared_queue.put([permu_list[idx + 1], stim_timestamp, 0])
-
+                shared_queue.put([idx, stim_timestamp, trial_multiplier])
                 win.flip()
-                core.wait(numsecs) #random interval seconds
-                
-
-                # #introduce sound for eye close after seeing the finger then wait for the beep to start task
-                # beep1.play()
-                # core.wait(1) #1 seconds 
-                # beep1.stop()
-                # core.wait(3)#3 second
-                # beep1.play() # second time play peep stop and then open eyes 
-                # core.wait(1)
-                # beep1.stop()
-
-                #while timer.getTime() < numsecs:
-                #passtime.sleep(numsecs)  # Display picture for 3 second.
-
-                if hasbreak == True:
-                    system_time = time.time()  # Get the current system time
-                    stim_timestamp = system_time - time_diff  # Calculate the stimulus onset time using the 
-                    shared_queue.put([100,stim_timestamp,break_interval])
-                    emptystim.draw() 
-                    win.flip()
-                    # break_multiplier = random.randint(1, break_interval)  # This will give you a number between 1 and 8
-                    # breaktime = break_multiplier * 0.5 
-                     # rest for 1 seconds between trials 
-                    core.wait(break_interval*0.5)
-                    #time.sleep(0.1)
-
+                core.wait(1) #random interval seconds
+            if hasbreak == True:
+                stim_timestamp = system_time - time_diff  # Calculate the stimulus onset time using the 
+                shared_queue.put([100,stim_timestamp,break_interval])
+                emptystim.draw() 
+                # break_multiplier = random.randint(1, break_interval)  # This will give you a number between 1 and 8
+                # breaktime = break_multiplier * 0.5 
+                    # rest for 1 seconds between trials 
+                core.wait(break_interval)
+                #time.sleep(0.1)
             print("get to save")
             system_time = time.time()  # Get the current system time
             stim_timestamp = system_time - time_diff
@@ -212,33 +157,26 @@ def main():
     if testing_with_wifi:
         eeg_thread = threading.Thread(target=runstream, args=(eeg_inlet,eeg_num_samples*len(permu_list)))
         eeg_thread.start()
-
-
     for i, stim_list in enumerate(stim_matrix):
         if testing_with_wifi:
             sample, timestamp = eeg_inlet.pull_sample()
             start_time=time.time()
             eeg_timestamp = timestamp
             time_diff = start_time - eeg_timestamp
-        else:
             time_diff = 0
-        run_exp(exp_names[i], stim_list, numexp=numexp, win=win, shuffle=True,hasbreak=True,emptystim=emptystim,timedif=time_diff,break_interval=2,trial_interval=4)
+        print(stim_list)
+        run_exp(exp_names[i], stim_list, numexp=numexp, win=win, shuffle=True,hasbreak=True,emptystim=emptystim,timedif=time_diff,break_interval=4,trial_interval=2)
         print(i, "experiment")
         core.wait(5)
-    
-
     print("haved looped through all tasks")
     exp_running = False
     print("done")
     win.close()
     core.quit()
-
-
-def runstream(eeg_inlet, num_samples, batch_size=1,exp_name="doublefinger_1sec"):
+def runstream(eeg_inlet, num_samples, batch_size=1,exp_name="asecondhandsao_meg"):
     global exp_running
     if eeg_inlet is None:  # Do nothing if there's no EEG inlet (i.e., in testing mode)
         return
-
     # Initialize variables
     currentmarker = 0
     data_batch = []
@@ -250,37 +188,28 @@ def runstream(eeg_inlet, num_samples, batch_size=1,exp_name="doublefinger_1sec")
     durations=[]
     duration_channel=[]
     tempmarker=[]
-    
-
     # Check if file exists outside of the loop
     filename_template = "data_{}.csv"
     file_mode = 'ab' if os.path.isfile(filename_template.format("placeholder")) else 'ab'
-
     while exp_running:
         # Read from the queue
         if not shared_queue.empty():
             currentmarker_timestamp = shared_queue.get()
-            
             if currentmarker_timestamp[0] != 200:
                 tempmarker.append(currentmarker_timestamp[0])
                 stim_timestamps.append(currentmarker_timestamp[1])
                 durations.append(currentmarker_timestamp[2])
                 continue
-
             # Read a chunk of EEG and AUX data
             eeg_chunk, timestamp = eeg_inlet.pull_chunk(max_samples=num_samples + 150)
-
-            filename = filename_template.format("doubleprocess")
-
+            filename = filename_template.format(exp_name)
             data = np.hstack([
                 np.array(timestamp).reshape(-1, 1),
                 eeg_chunk
             ])
-
             # Add to data batch
             data_batch.append(data)
             counter += 1
-
             if counter >= batch_size:
                 # Save data batch to CSV file
                 with open(filename, file_mode) as f:
@@ -297,18 +226,13 @@ def runstream(eeg_inlet, num_samples, batch_size=1,exp_name="doublefinger_1sec")
                 stim_timestamps=[]
                 tempmarker = []
                 durations = []
-
                 counter = 0
-
-
     # Save any remaining data in the batch after loop ends
     if data_batch:
         with open(filename, file_mode) as f:
             for batch in data_batch:
                 np.savetxt(f, batch, delimiter=",", fmt=['%f'] + ['%f'] * 8 + ['%s']+['%f']+[ '%f'])
-
 # '''''''
 if __name__ == "__main__":
     # Run the run() functiona
     main()
-
